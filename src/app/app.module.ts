e38164231e8a127e8a15b4f64e9d52f495b91d45
RohanGautam <rohan1gautam@gmail.com>
2020-09-06 10:47:12 +0800

    added partners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,9 +16,11 @@ import { Section1Component } from './section1/section1.component';
 import { ImgTextVerticalComponent } from './components/img-text-vertical/img-text-vertical.component';
 import { Section2Component } from './section2/section2.component';
 import { PilotTrailsComponent } from './pilot-trails/pilot-trails.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, Section1Component, ImgTextVerticalComponent, Section2Component, PilotTrailsComponent],
+  declarations: [AppComponent, HeroComponent, Section1Component, ImgTextVerticalComponent, Section2Component, PilotTrailsComponent, PartnersComponent, ContactComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/components/img-text-vertical/img-text-vertical.component.ts
+++ src/app/components/img-text-vertical/img-text-vertical.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ImgTextVerticalComponent implements OnInit {
   // image = 'assets/images/plot-viewer.png';
   @Input() image: string;
-  @Input() title: string = '';
   @Input() text: string;
+  @Input() title: string = '';
+  @Input() imgWidth: string = '100px';
   // text =
   //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
   // text = 'Lorem ipsum dolor sit amet';
