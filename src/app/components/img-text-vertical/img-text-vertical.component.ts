87013f1300e60038fb8720d9286a7969ba0dda39
RohanGautam <rohan1gautam@gmail.com>
2020-09-10 16:24:01 +0800

    make partner tiles equal

--- src/app/components/img-text-vertical/img-text-vertical.component.ts
+++ src/app/components/img-text-vertical/img-text-vertical.component.ts
@@ -10,7 +10,7 @@ export class ImgTextVerticalComponent implements OnInit {
   @Input() image: string;
   @Input() text: string = '';
   @Input() title: string = '';
-  @Input() imgWidth: string = '100px';
+  @Input() imgHeight: string = '100px';
   // text =
   //   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
   // text = 'Lorem ipsum dolor sit amet';
